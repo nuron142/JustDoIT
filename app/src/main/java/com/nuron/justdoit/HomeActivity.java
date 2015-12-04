2bc9a8fc9c53a27d9a287893b697fc0adab0b46b
Sunil <sunil.k@swiggy.in>
2015-12-05 05:21:24 +0530

    Navigation view text update

--- app/src/main/java/com/nuron/justdoit/HomeActivity.java
+++ app/src/main/java/com/nuron/justdoit/HomeActivity.java
@@ -1,15 +1,17 @@
 package com.nuron.justdoit;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.parse.ParseUser;
 
@@ -34,6 +36,22 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        View header = navigationView.getHeaderView(0);
+        TextView userNameTextView = (TextView) header.findViewById(R.id.userName);
+        TextView userEmailTextView = (TextView) header.findViewById(R.id.userEmail);
+
+        ParseUser parseUser = ParseUser.getCurrentUser();
+        if (parseUser != null) {
+            String userName = parseUser.getString(LoginActivity.USER_ACCOUNT_NAME);
+            String userEmail = parseUser.getEmail();
+            if(userName != null) {
+                userNameTextView.setText(userName);
+            }
+
+            if(userEmail != null) {
+                userEmailTextView.setText(userEmail);
+            }
+        }
     }
 
     @Override
